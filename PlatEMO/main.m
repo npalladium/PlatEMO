b3683c0e62a225878fa56477a29c722aed5a7dd4
nikhil <ch17btech11023@iith.ac.in>
2021-03-20 22:29:38 +0530

    Update main script to work with DBMOPPa

--- PlatEMO/main.m
+++ PlatEMO/main.m
@@ -4,7 +4,7 @@ theta = linspace(0.1,1000,2);
 N = 100;
 objval = zeros(N*size(theta,2),3);
 for iterations=1:size(theta,2)
-    [~,objval(N*(iterations-1)+1:N*iterations,:),~] = platemo('algorithm',{@SMSEMOA,1,theta(iterations)},'problem',@DBMOPP);
+    [~,objval(N*(iterations-1)+1:N*iterations,:),~] = platemo('algorithm',{@SMSEMOA,1,theta(iterations)},'problem',@DBMOPPa,'M',3,'D',3,'N',N);
 end
 
 % Generating a true PF estimate - Since the true PF is not given ; using Non dominated sorting
